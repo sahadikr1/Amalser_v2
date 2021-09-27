4447c9bf97dbbcdf26bd53aee5d9f49734419140
sahadikr1 <63252901+sahadikr1@users.noreply.github.com>
2021-09-28 05:20:11 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Amalser*:  ```" + msg + "```"
+    return "✅ *sahad ikr*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Amalser*:  ```" + msg + "```"
+    return "🛑 *sahad ikr*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Amalser*:  ```" + msg + "```"
+    return "⏺️ *sahad ikr*:  ```" + msg + "```"
 }
 
 
