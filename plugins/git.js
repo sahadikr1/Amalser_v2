0d3835eb85e8339c7ee1b34c3c76a95a93bc30ae
sahadikr1 <63252901+sahadikr1@users.noreply.github.com>
2021-09-28 07:19:59 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -5,29 +5,20 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'owner', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
     var r_text = new Array ();
     
     
    
-  r_text[0] = "https://i.imgur.com/yCYYyEJ.jpeg";
+  r_text[0] = "https://i.imgur.com/ROijpOL.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
-*owner number wa.me/919895828468*
-
-*Bot making video : https://youtu.be/fd0KPblop-k*
-
-*Yt_PASSWORD: _________(password on this video watch it full)*
-
-*githublink : https://github.com/cyberchekuthan/Amalser_v2*
-
-*audio commads : https://github.com/cyberchekuthan/Amalser_v2/tree/master/uploads*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*created by sahad.ikr http://instagram.com/sahad.ikr
 `}) 
 
 }));
